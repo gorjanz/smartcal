803713ac5dedaac6f339d48d8ef1db4cc7664941
gorjanz <gorjanz@gmail.com>
2014-09-28 20:36:14 +0200

    - first implementation of controllers

--- SmartCal/src/com/smartcal/controllers/EventsController.java
+++ SmartCal/src/com/smartcal/controllers/EventsController.java
@@ -1,10 +1,66 @@
 package com.smartcal.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.smartcal.models.Event;
+import com.smartcal.services.EventsService;
 
 @Controller
 public class EventsController {
 
-	
-	
+	@Autowired
+	private EventsService eventsService;
+
+	public EventsController(EventsService eventsService) {
+		super();
+		this.eventsService = eventsService;
+	}
+
+	@RequestMapping(value = "/events", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<Event> getEvents() {
+		return eventsService.getEvents();
+	}
+
+	@RequestMapping(value = "/events/{eventid}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody Event getById(@PathVariable int eventid) {
+		return eventsService.getById(eventid);
+	}
+
+	@RequestMapping(value = "/users/{userid}/past", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<Event> getPastEventsForUser(
+			@PathVariable int userid) {
+		return eventsService.getPastEvents(userid);
+	}
+
+	@RequestMapping(value = "/users/{userid}/future", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<Event> getFutureEventsForUser(
+			@PathVariable int userid) {
+		return eventsService.getIncomingEvents(userid);
+	}
+
+	@RequestMapping(value = "/events/{eventid}/attend", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void attend(int userid, int eventid) {
+		eventsService.attend(userid, eventid);
+	}
+
+	@RequestMapping(value = "/events/{eventid}/cancel", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void cancel(int userid, int eventid) {
+		eventsService.cancel(userid, eventid);
+	}
+
 }
--- SmartCal/src/com/smartcal/controllers/UsersController.java
+++ SmartCal/src/com/smartcal/controllers/UsersController.java
@@ -1,8 +1,66 @@
 package com.smartcal.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.smartcal.models.User;
+import com.smartcal.services.UsersService;
 
 @Controller
 public class UsersController {
+
+	@Autowired
+	private UsersService usersService;
+
+	public UsersController(UsersService usersService) {
+		super();
+		this.usersService = usersService;
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<User> getUsers() {
+		return usersService.getUsers();
+	}
+
+	@RequestMapping(value = "/users/{userid}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody User getById(@PathVariable int userid) {
+		return usersService.getUserById(userid);
+	}
+
+	@RequestMapping(value = "/users/add", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addUser(User usr) {
+		usersService.registerNewUser(usr);
+	}
+
+	@RequestMapping(value = "/events/{eventid}/users")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<User> getAttendingForEvent(
+			@PathVariable int eventid) {
+		return usersService.getAttendies(eventid);
+	}
+
+	@RequestMapping(value = "/{userid}/{eventid}/friends")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<User> getFriendsAttending(
+			@PathVariable int userid, @PathVariable int eventid) {
+		return usersService.getFriendsAttending(userid, eventid);
+	}
+	
+	@RequestMapping(value = "/users/{userid}/friends", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody List<User> getFriends(@PathVariable int userid) {
+		return usersService.getFriendsFor(userid);
+	}
 
 }
